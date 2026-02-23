518f18b8ef2cfef03e8133c0cea0f0ddf43686c9
nemcevaanna <90156836+nemcevaanna@users.noreply.github.com>
2026-02-23 15:58:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,15 +6,18 @@ import numpy as np
 import cv2
 import io
 from PIL import Image
+import gdown
 
 app = FastAPI(title="Image Classifier API")
 
 # Загрузка модели
-MODEL_PATH = load_model("model/T5_3_best_model.h5")
+FILE_ID = os.getenv('GOOGLE_DRIVE_FILE_ID', '1eppWxpU2WVVsxw4RGx4Au3n7I84IjkPZ')
+MODEL_PATH = "model/T5_3_best_model.h5"
 
 if not os.path.exists(MODEL_PATH):
     os.makedirs("model", exist_ok=True)
-    gdown.download("https://drive.google.com/uc?id=1eppWxpU2WVVsxw4RGx4Au3n7I84IjkPZ", MODEL_PATH, quiet=False)
+    url = f"https://drive.google.com/uc?id={FILE_ID}"
+    gdown.download(url, MODEL_PATH, quiet=False)
 
 model = load_model(MODEL_PATH)
 
